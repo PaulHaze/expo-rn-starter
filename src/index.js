36970cbe6b9297333222cd288f0669971e0e4f1f
PaulHaze <dev@artemist.com.au>
2022-04-06 16:36:23 +0100

    add icons package

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-import { Test } from "./components";
+import { Test } from './components';
 
 export default function App() {
   return (
@@ -17,8 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
