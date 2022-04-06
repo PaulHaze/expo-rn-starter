09f247d73b905eeddf3d4ffeff4a0c7cdce84aaf
PaulHaze <dev@artemist.com.au>
2022-04-06 17:43:39 +0100

    removed old components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-import { Test } from './components';
-
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Text>Delete This And Start Building Something</Text>
-      <Test />
     </View>
   );
 }
